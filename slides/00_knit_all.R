38cca135483dc1ab1e944c2c6eb2868423ffde3d
Kvaled1 <dorian.l.kvale@state.mn.us>
2023-11-09 12:25:03 -0600

    switch to monsters

---
+++ slides/00_knit_all.R
@@ -0,0 +1,23 @@
+library(rmarkdown)
+
+knit_to_netlify <- function(day, rmd_file) {
+
+  tryCatch(setwd(paste0("slides/", day)),
+           error = function(e) NA)
+
+  render(rmd_file)
+
+  html_file <- gsub(".Rmd", ".html", rmd_file)
+
+  file.copy(from = html_file,
+            to = paste0("../netlify/", day, "/", html_file),
+            overwrite = TRUE)
+
+  file.remove(html_file)
+}
+
+
+rmds <- list(c("day1", "01-3-R-bakery.Rmd"),
+             c("day1", "01-4-Tables-and-dataframes.Rmd"))
+
+sapply(rmds, function(x) knit_to_netlify(x[1], x[2]))
